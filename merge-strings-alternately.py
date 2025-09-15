c76039f4e666388a9b54ba6bd8c099966a732f15
Hermii1 <hermelaejigu4@gmail.com>
2025-09-15 22:09:42 +0300

    merge-strings-alternately

---
+++ merge-strings-alternately.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        i, j = 0, 0
+        result = []
+        len1, len2 = len(word1), len(word2)
+        while i < len1 or j < len2:
+            if i < len1:
+                result.append(word1[i])
+                i += 1
+            if j < len2:
+                result.append(word2[j])
+                j +=1
+        return ''.join(result)
